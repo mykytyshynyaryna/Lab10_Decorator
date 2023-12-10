07147b300344399a765eb4c0c90d4995c0dddd94
mykytyshynyaryna <116551823+mykytyshynyaryna@users.noreply.github.com>
2023-12-10 20:12:49 +0200

    .

--- src/main/java/ua/edu/ucu/apps/adapter/task2/Client.java
+++
@@ -1,9 +0,0 @@
-package ua.edu.ucu.apps.adapter.task2;
-
-import java.time.LocalDate;
-
-public interface Client {
-    String getEmail();
-    String getCountry();
-    LocalDate getLastActiveTime();
-}
--- src/main/java/ua/edu/ucu/apps/adapter/task3/Authorization.java
+++
@@ -1,7 +0,0 @@
-package ua.edu.ucu.apps.adapter.task3;
-
-public class Authorization extends Авторизація {
-    public boolean authorize(БазаДаних db) {
-        return авторизуватися(db);
-    }
-}
