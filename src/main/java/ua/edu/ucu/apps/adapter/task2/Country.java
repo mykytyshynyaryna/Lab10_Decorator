e713e26d84e729c14f375da62f082b6ea3883326
yarynamykytyshyn <mykytyshyn.pn@ucu.edu.ua>
2023-12-10 20:15:03 +0200

    del

--- src/main/java/ua/edu/ucu/apps/adapter/task2/Country.java
+++
@@ -1,5 +0,0 @@
-package ua.edu.ucu.apps.adapter.task2;
-
-public enum Country {
-    UKRAINE, USA, GERMANY, FRANCE, UK, CHINA, JAPAN, CANADA, AUSTRALIA
-}
--- src/main/java/ua/edu/ucu/apps/adapter/task3/Авторизація.java
+++
@@ -1,8 +0,0 @@
-package ua.edu.ucu.apps.adapter.task3;
-
-public class Авторизація {
-    public boolean авторизуватися(БазаДаних db) {
-        db.отриматиДаніКористувача();
-        return true;
-    }
-}
